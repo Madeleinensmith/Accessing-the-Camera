20c31a64a737f78ce498a2da8350f8747af74b26
Madeleine Smith <madeleine.n.smith@gmail.com>
2020-06-23 13:26:11 -0500

    final

--- Accessing the Camera/ViewController.swift
+++ Accessing the Camera/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var displayImage: UIImageView!
+    
+    
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
 
+    
+
+    @IBAction func takeSelfieTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+           present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func photoLibraryTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            displayImage.image = selectedImage
+            
+        }
+            imagePicker.dismiss(animated: true, completion: nil)
+    }
 
 }
 
